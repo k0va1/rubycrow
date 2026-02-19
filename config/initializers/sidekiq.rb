a449a5c6b31b214aefc355791a597b5a816c23d3
Alex Koval <al3xander.koval@gmail.com>
2026-02-19 12:20:43 +0100

    feat: configure sidekiq scheduler per environment
    Move schedule definitions from sidekiq.yml into per-environment
    files under config/scheduler/. Load the appropriate schedule on
    Sidekiq startup. Disable parse_rss_feeds in development.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,14 @@
 Sidekiq.configure_server do |config|
   config.redis = {url: Rails.application.credentials.redis_url}
+
+  config.on(:startup) do
+    schedule_file = Rails.root.join("config/scheduler/#{Rails.env}.yml")
+
+    if File.exist?(schedule_file)
+      Sidekiq.schedule = YAML.load_file(schedule_file)
+      SidekiqScheduler::Scheduler.instance.reload_schedule!
+    end
+  end
 end
 
 Sidekiq.configure_client do |config|
